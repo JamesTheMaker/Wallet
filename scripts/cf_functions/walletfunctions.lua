9d305c6b957b5794042e58769391e72d027c18b9
JamesTheMaker <jamesthemaker2005@gmail.com>
2024-02-12 17:24:31 -0500

    QoL update

--- scripts/cf_functions/walletfunctions.lua
+++ scripts/cf_functions/walletfunctions.lua
@@ -14,9 +14,12 @@ function printCurrencies()
     cfg = root.assetJson("/currencies.config")
     str = "Your wallet contains:\n"
 
-    for currency, _ in pairs(cfg) do
-        if player.currency(currency) > 0 then
-            str = str .. currency .. ": " .. player.currency(currency) .. "\n"
+    for currency, dat in pairs(cfg) do
+        item = root.itemConfig(dat.representativeItem).config
+        hidden = dat.sail_hidden
+
+        if player.currency(currency) > 0 and not hidden then
+            str = str .. item.shortdescription .. ": " .. player.currency(currency) .. "\n"
         end
     end
 
